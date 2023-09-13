3622936b40d8196769075e71255342eaad422b37
sonsalmon <sjmjis0208@gmail.com>
2023-09-13 18:39:04 +0900

    Update book_detail_screen_widget.dart
    도서 상세창에서 도서 추가 및 삭제 기능 추가

--- lib/screens/book_detail_screen/book_detail_screen_widget.dart
+++ lib/screens/book_detail_screen/book_detail_screen_widget.dart
@@ -80,16 +80,99 @@ class _BookDetailScreenWidgetState extends State<BookDetailScreenWidget> {
                   ),
                   if (widget.inMyLibrary)
                     InkWell(
-                      onTap: () {}, //내 서재에서 삭제
+                      onTap: () async {
+                        final result = await showDialog<bool>(
+                          context: context,
+                          builder: (context) => AlertDialog(
+                            title: Text('삭제 확인'),
+                            content: Text(
+                                '정말 이 책을 서재에서 삭제할까요?\n 작성한 노트와 독서 기록들도 삭제됩니다.'),
+                            actions: [
+                              TextButton(
+                                onPressed: () =>
+                                    Navigator.of(context).pop(false),
+                                child: Text(
+                                  '아니요',
+                                  style: TextStyle(
+                                      color: FlutterFlowTheme.of(context)
+                                          .primaryText),
+                                ),
+                              ),
+                              TextButton(
+                                onPressed: () =>
+                                    Navigator.of(context).pop(true),
+                                child: Text(
+                                  '삭제',
+                                  style: TextStyle(
+                                      color:
+                                          FlutterFlowTheme.of(context).error),
+                                ),
+                              ),
+                            ],
+                          ),
+                        );
+                        if (result == true) {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                              content: Text(
+                                '도서 삭제 완료',
+                                style: FlutterFlowTheme.of(context)
+                                    .bodyMedium
+                                    .override(
+                                      fontFamily: 'Readex Pro',
+                                      color: FlutterFlowTheme.of(context)
+                                          .primaryText,
+                                    ),
+                              ),
+                              duration: Duration(milliseconds: 1000),
+                              backgroundColor:
+                                  FlutterFlowTheme.of(context).success,
+                            ),
+                          );
+                          //api call : 내 서재에서 삭제
+                          context.safePop();
+                        }
+                      }, //내 서재에서 삭제
                       child: Icon(
                         Icons.delete_outline,
                         color: FlutterFlowTheme.of(context).error,
                         size: 30,
                       ),
                     ),
-                  if (!widget.inMyLibrary)
+                  if (!widget.inMyLibrary) // 다른 사람 서재에서 책 추가
                     InkWell(
-                      onTap: () {}, //내 서재에 추가
+                      onTap: () async {
+                        final value = await showModalBottomSheet(
+                          isScrollControlled: true,
+                          backgroundColor: Colors.transparent,
+                          enableDrag: false,
+                          context: context,
+                          builder: (context) {
+                            return SelectReadingStateWidget();
+                          },
+                        );
+                        if (value != null) {
+                          //독서 상태를 선택했으면
+                          //api call 내 서재에 추가
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                              content: Text(
+                                '서재에 추가 완료!',
+                                style: FlutterFlowTheme.of(context)
+                                    .bodyMedium
+                                    .override(
+                                      fontFamily: 'Readex Pro',
+                                      color: FlutterFlowTheme.of(context)
+                                          .primaryText,
+                                    ),
+                              ),
+                              duration: Duration(milliseconds: 1000),
+                              backgroundColor:
+                                  FlutterFlowTheme.of(context).success,
+                            ),
+                          );
+                        }
+                      }, //내 서재에 추가
                       child: Icon(
                         Icons.add,
                         color: FlutterFlowTheme.of(context).primaryText,
@@ -150,40 +233,41 @@ class _BookDetailScreenWidgetState extends State<BookDetailScreenWidget> {
                         style: FlutterFlowTheme.of(context).labelMedium,
                       ),
                     ),
-                    Padding(
-                      padding:
-                          EdgeInsetsDirectional.fromSTEB(16.0, 8.0, 0.0, 0.0),
-                      child: InkWell(
-                        splashColor: Colors.transparent,
-                        focusColor: Colors.transparent,
-                        hoverColor: Colors.transparent,
-                        highlightColor: Colors.transparent,
-                        onTap: () async {
-                          // 가져온 걸로 업데이트
-                          //
-                          await showModalBottomSheet(
-                            isScrollControlled: true,
-                            backgroundColor: Colors.transparent,
-                            enableDrag: false,
-                            context: context,
-                            builder: (context) {
-                              return Padding(
-                                padding: MediaQuery.viewInsetsOf(context),
-                                child: SelectReadingStateWidget(),
-                              );
-                            },
-                          ).then((value) =>
-                              setState(() => _model.readingState = value));
+                    if (widget.inMyLibrary)
+                      Padding(
+                        padding:
+                            EdgeInsetsDirectional.fromSTEB(16.0, 8.0, 0.0, 0.0),
+                        child: InkWell(
+                          splashColor: Colors.transparent,
+                          focusColor: Colors.transparent,
+                          hoverColor: Colors.transparent,
+                          highlightColor: Colors.transparent,
+                          onTap: () async {
+                            // 가져온 걸로 업데이트
+                            //
+                            await showModalBottomSheet(
+                              isScrollControlled: true,
+                              backgroundColor: Colors.transparent,
+                              enableDrag: false,
+                              context: context,
+                              builder: (context) {
+                                return Padding(
+                                  padding: MediaQuery.viewInsetsOf(context),
+                                  child: SelectReadingStateWidget(),
+                                );
+                              },
+                            ).then((value) =>
+                                setState(() => _model.readingState = value));
 
-                          setState(() {});
-                        },
-                        child: Text(
-                          Constants.readingState[_model.readingState] ??
-                              '독서 상태',
-                          style: FlutterFlowTheme.of(context).labelMedium,
+                            setState(() {});
+                          },
+                          child: Text(
+                            Constants.readingState[_model.readingState] ??
+                                '독서 상태',
+                            style: FlutterFlowTheme.of(context).labelMedium,
+                          ),
                         ),
                       ),
-                    ),
                     if (_model.readingState == 'finish')
                       Padding(
                         padding: const EdgeInsets.fromLTRB(12, 8, 0, 0),
